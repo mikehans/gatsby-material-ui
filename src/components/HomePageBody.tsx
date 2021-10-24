a52d263fe16e95a04ed077656fe4f33cdd997ca4
Mike Hansford <mjhansford@gmail.com>
2021-10-24 12:48:53 +1100

    Text change. Formatting change

--- src/components/HomePageBody.tsx
+++ src/components/HomePageBody.tsx
@@ -5,52 +5,60 @@ import React from "react";
 import Hero from "./Hero";
 import HeroBox from "./HeroBox";
 
-function HomePageBody( data ) {
-    const {title, subtitle} = data;
+function HomePageBody(data) {
+  const { title, subtitle } = data;
 
-    return (
+  return (
     <>
-      <Box component="div" m={1} sx={{ marginTop: 70, marginLeft: 0, bgcolor: 'secondary.main' }}>
+      <Box
+        component="div"
+        m={1}
+        sx={{ marginTop: 70, marginLeft: 0, bgcolor: "secondary.main" }}
+      >
         <Typography variant="h1">{title}</Typography>
-        <Typography variant="subtitle1">
-         {subtitle}
-        </Typography>
+        <Typography variant="subtitle1">{subtitle}</Typography>
       </Box>
 
       <Hero />
       <HeroBox />
 
       <Grid container spacing={2}>
-      <Grid item xs={12}>
+        <Grid item xs={12}>
           <Paper>
-              Hi I'm Mike. I'm a software developer, family man, woodworker, gardener and electronics hack.
-              <Link to="/about" component={GatsbyLink}>Link Learn more</Link>
+            No, my father didn't fight in the wars. He was a navigator on a
+            spice freighter.
+            <Link to="/about" component={GatsbyLink}>
+              Link Learn more
+            </Link>
           </Paper>
         </Grid>
         <Grid item xs={12} lg={4}>
           <Paper>
-              Read my tech blog
-              <Link to="/blog" component={GatsbyLink}>Link Learn more</Link>
+            Read my blog
+            <Link to="/blog" component={GatsbyLink}>
+              Link Learn more
+            </Link>
           </Paper>
         </Grid>
         <Grid item xs={12} md={8} lg={4}>
-            <Paper>
-                Tech projects
-              <Link to="/projects" component={GatsbyLink}>Link Learn more</Link>
-            </Paper>
+          <Paper>
+            Tech projects
+            <Link to="/projects" component={GatsbyLink}>
+              Link Learn more
+            </Link>
+          </Paper>
         </Grid>
         <Grid item xs={12} md={4} lg={4}>
-            <Paper>
-                Other stuff
-              <Link to="/other-interests" component={GatsbyLink}>Link Learn more</Link>
-            </Paper>
+          <Paper>
+            Other stuff
+            <Link to="/other-interests" component={GatsbyLink}>
+              Link Learn more
+            </Link>
+          </Paper>
         </Grid>
-
       </Grid>
     </>
   );
 }
-
-
 
 export default HomePageBody;
